111a5ed42425a52ab9c5c40c3223743abd6b9076
devnet-cloud <70343564+devnet-cloud@users.noreply.github.com>
2020-09-23 16:50:38 +0200

    Create loging_request.py

---
+++ Nexus/REST/loging_request.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+"""Sample of Get Interface
+"""
+
+# Import libraries
+import requests
+import sys
+import json
+from pprint import pprint
+
+# Use self signed certs
+requests.packages.urllib3.disable_warnings()
+
+# Credentials
+USER = 'admin'
+PASS = 'cisco123'
+
+# URL for GET request
+url= "https://10.10.20.58/api/mo/aaaLogin.json"
+
+payload = "{\r\n    \"aaaUser\":{\r\n        \"attributes:{\r\n            \"name\":\"admin\",\r\n            \"pwd\":\"Cisco123\"\r\n        }\r\n    }\r\n}"
+
+# Set yang+jsonas the data formats
+headers = {'Content-Type': 'application/yang-data+json',
+           'Accept': 'application/yang-data+json'}
+           
+# Run POST
+response = requests.request("POST",url, auth=(USER, PASS),headers=headers, data=payload,  verify=False)
+response_json = response.json()
+token=(response_json["imdata"][0]["aaaLogin"]["attributes"]["token"])
+
+# Print results
+print(token)
